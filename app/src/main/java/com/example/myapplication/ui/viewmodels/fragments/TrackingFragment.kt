56f31a3f262dff336cfd94fbbbeab630987ea9dc
alemoore <alexey.murnikoff@yandex.ru>
2020-11-16 12:57:38 +0300

    navigation setup

--- app/src/main/java/com/example/myapplication/ui/viewmodels/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/viewmodels/fragments/TrackingFragment.kt
@@ -1,7 +1,18 @@
 package com.example.myapplication.ui.viewmodels.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
+import kotlinx.android.synthetic.main.fragment_run.*
 
 class TrackingFragment: Fragment(R.layout.fragment_tracking) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_trackingFragment_to_runFragment)
+        }
+    }
 }
